b53143689a3899cd737b909b1668f613e0f8d72e
Subhajit Ghosh <subhajit2004jit@gmail.com>
2026-04-28 22:00:27 +0530

    Insertion Sort

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,46 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+    // Method for Insertion Sort
+    static void sortArray(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = temp;
+        }
+    }
+
+    // Method to print array
+    static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        sortArray(arr);  
+        
+        
+        System.out.print("Print the array after Insertion sort: ");
+        printArray(arr);    
+    }
+}
